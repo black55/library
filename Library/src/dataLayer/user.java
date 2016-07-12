5e879f3bc352c7c5bc6b9bace6736ee8df3957d1
irshad <irshadichu00@gmail.com>
2016-07-12 21:38:17 +0530

    communication handling

--- Library/src/dataLayer/user.java
+++ Library/src/dataLayer/user.java
@@ -1,6 +1,10 @@
 package dataLayer;
 import java.sql.*;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class user {
 	
 	public static boolean isUserExist(String tableName, String userName)
@@ -11,7 +15,7 @@ public class user {
 		ResultSet res = null;
 		try
 		{
-			conn = dataLayer.getConnection.get();
+			conn = getConnection.get();
 			
 			if(tableName.equals("user"))
 				stmt = conn.prepareStatement("select  from user where username = ?");
@@ -37,9 +41,32 @@ public class user {
 		return returnValue;
 	}
 	
-	public static boolean addNewUser()
+	public static boolean addNewUser(HttpServletRequest userInfo)
 	{  
-		return true;
+		boolean returnValue = false;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		try
+		{
+			conn = getConnection.get();
+			stmt = conn.prepareStatement("insert into tempuser (username, password, email, rollid, mobile, testkey) values (?,?,?,?,?,?)");
+			stmt.setString(1, userInfo.getParameter("username"));
+			stmt.setString(2, userInfo.getParameter("password"));
+			stmt.setString(3, userInfo.getParameter("email"));
+			stmt.setString(4, userInfo.getParameter("roll"));
+			stmt.setString(5, userInfo.getParameter("country").toString().concat(userInfo.getParameter("mobile")));
+			stmt.setString(6, RandomStringUtils.randomAlphanumeric(15));
+			stmt.executeUpdate();
+			returnValue = true;
+		}
+		catch(SQLException e)
+		{
+		}
+		finally
+		{
+			exceptionHandler.sqlSafeClose.closeAll(conn,stmt,null);
+		}
+		return returnValue;
 	}
 	
 	
--- Library/src/userManagement/signup.java
+++ Library/src/userManagement/signup.java
@@ -2,6 +2,7 @@ package userManagement;
 
 import java.io.*;
 import java.sql.*;
+import java.util.HashMap;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -27,51 +28,27 @@ public class signup extends HttpServlet{
 		}
 		else
 		{
-			Connection conn = null;
-			PreparedStatement stmt = null;
-			ResultSet rs = null;
 			String username = req.getParameter("username");
-			String email = req.getParameter("email");
-			String roll = req.getParameter("roll");
-			String country = req.getParameter("country");
-			String mobile = req.getParameter("mobile");
-			String pass = req.getParameter("password");
-			try
+			
+			if(user.isUserExist("user", username))
 			{
-				
-				if(user.isUserExist("user", username))
-				{
-					PrintWriter writer = res.getWriter();
-					writer.write("user exist");
-				}
-				else if(user.isUserExist("tempuser", username))
-				{
-					PrintWriter writer = res.getWriter();
-					writer.write("user already registered ... please verify email ...");
-				}
-				else
-				{
-					exceptionHandler.sqlSafeClose.closeAll(null,stmt,rs);
-					stmt = conn.prepareStatement("insert into tempuser (username, password, email, rollid, mobile, testkey) values (?,?,?,?,?,?)");
-					stmt.setString(1, username);
-					stmt.setString(2, pass);
-					stmt.setString(3, email);
-					stmt.setString(4, roll);
-					stmt.setString(5, country.toString().concat(mobile));
-					String testkey = RandomStringUtils.randomAlphanumeric(15);
-					stmt.setString(6,testkey);
-					stmt.executeUpdate();
-					PrintWriter writer = res.getWriter();
-					writer.write("user inserted");
-				}
+				PrintWriter writer = res.getWriter();
+				writer.write("user exist");
 			}
-			catch(SQLException e)
-			{			
-				System.out.println(e);
-			}
-			finally
+			else if(user.isUserExist("tempuser", username))
 			{
-				exceptionHandler.sqlSafeClose.closeAll(conn,stmt,rs);
+				PrintWriter writer = res.getWriter();
+				writer.write("user already registered ... please verify email ...");
+			}
+			else if(user.addNewUser(req))
+			{
+				PrintWriter writer = res.getWriter();
+				writer.write("user inserted");
+			}
+			else
+			{
+				PrintWriter writer = res.getWriter();
+				writer.write("user insertion failed ... please try again ..");
 			}
 		}
 	}
